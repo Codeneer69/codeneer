617874a1269df7efacbe7ac3544836c15aaa4a92
Atharva <atharvameshram06@gmail.com>
2020-12-02 22:18:38 +0530

    You have been hacked

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
+class Post(models.Model):
+    serial_number = models.AutoField(primary_key = True)
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    timeStamp = models.DateTimeField(blank=True)
+    
+    def __str__(self):
+        return  self.title + ' by ' + self.author
